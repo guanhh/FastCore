d728e3fa3831de7ebf57042f0f6a06368066d242
guanhh <guanhaohao@hotmail.com>
2021-08-10 16:10:48 +0800

    数据库迁移功能

---
+++ src/FastCore.DbMigration/DbMigratorHostedService.cs
@@ -0,0 +1,30 @@
+﻿using FastCore.EFCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FastCore.DbMigrations
+{
+    public class DbMigratorHostedService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DbMigratorHostedService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var context = _serviceProvider.GetService(typeof(FastCoreContext));
+            await (context as FastCoreContext).Database.MigrateAsync(cancellationToken: cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/FastCore.DbMigration/Program.cs
@@ -0,0 +1,31 @@
+﻿using FastCore.EFCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FastCore.DbMigrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).RunConsoleAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+          Host.CreateDefaultBuilder(args)
+              .ConfigureServices((context, services) =>
+              {
+                  var config = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                        .Build();
+
+                  var connectionString = config.GetConnectionString("SRMConnection");
+                  services.AddDbContext<FastCoreContext>(options => options.UseSqlServer(connectionString));
+
+                  services.AddHostedService<DbMigratorHostedService>();
+              });
+
+    }
+}
